a27b6ec190f9998e2ce30c42e541b3b4a6175ee3
Tancredi Carluccio <tanc_@hotmail.it>
2022-06-08 15:27:37 +0200

    Aggiunti componenti Vue

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Route::middleware('auth')
 ->prefix('admin') 
@@ -29,3 +23,7 @@ Route::middleware('auth')
     Route::get('/', 'HomeController@index')->name('home');
     Route::resource('posts', 'PostController');
 });
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+} )->where("any", ".*");
