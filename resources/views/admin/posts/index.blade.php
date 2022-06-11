13c5938ef82193b9d0d3865f3f3e33c131e6f121
Tancredi Carluccio <tanc_@hotmail.it>
2022-06-11 15:31:26 +0200

    Index creata

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,6 +2,34 @@
 
 @section('content')
 
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Content</th>
+            <th scope="col">Image</th>
+            <th scope="col">Slug</th>
+        </tr>
+        </thead>
+        <tbody>
+            @forelse ($posts as $post)
+
+            <tr>
+                <th scope="row">{{$post->title}}</th>
+                <td>{{$post->content}}</td>
+                <td> <img src="{{$post->title}}" alt=""> </td>
+                <td>{{$post->slug}}</td>
+              </tr>
+                
+            @empty
+
+            <h1>Non sono presenti post</h1>
+                
+            @endforelse
+        
+        </tbody>
+    </table>
+
 @endsection
 
 
